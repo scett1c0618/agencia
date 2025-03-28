b7d2cf622727cfe54ef949735160f44121ea2c47
scett1c0618 <scienza.e.arte1.618033@gmail.com>
2025-03-28 05:09:16 -0500

    initial repo2

--- obj/Debug/net8.0/appAgencia.AssemblyInfo.cs
+++ obj/Debug/net8.0/appAgencia.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appAgencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50699dc977a3ca69aaeaf850ca93e09d44604048")]
 [assembly: System.Reflection.AssemblyProductAttribute("appAgencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appAgencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
