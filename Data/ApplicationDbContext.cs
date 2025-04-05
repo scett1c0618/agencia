4fb31af85e7d73cd252febbd4902b1cc4fe37fcf
JeanEstrada-2004 <jeanestrada350@gmail.com>
2025-04-04 21:33:22 -0500

    Lista de Tous actualizada, ya funciona con la base de datos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using appAgencia.Models;
 
-namespace appAgencia.Data;
-
-public class ApplicationDbContext : IdentityDbContext
+namespace appAgencia.Data
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Region> Regiones { get; set; }
+        public DbSet<Destino> Destinos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuración de las relaciones
+            modelBuilder.Entity<Destino>()
+                .HasOne(d => d.Region)
+                .WithMany(r => r.Destinos)
+                .HasForeignKey(d => d.id_region);
+        }
     }
 }
